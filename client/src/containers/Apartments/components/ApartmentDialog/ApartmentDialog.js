ab5deffb7f177798fcaf7b175b1123d537328a0b
superdev807 <charles.ferria21@gmail.com>
2020-06-19 21:36:58 +1000

    Apartment Dialog Design complete

--- client/src/containers/Apartments/components/ApartmentDialog/ApartmentDialog.js
+++ client/src/containers/Apartments/components/ApartmentDialog/ApartmentDialog.js
@@ -1,18 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Dialog, TextField, RadioGroup, FormControlLabel, Radio } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save';
 import { DialogTitle, DialogContent, DialogActions } from 'components/DialogSubComponents';
 import NumberFormatCustom from 'components/NumberFormatCustom';
 import clsx from 'clsx';
+import validate from 'validate.js';
 import { useStyles } from './styles';
+
+const schema = {
+    name: {
+        presence: { allowEmpty: false, message: 'is required' },
+    },
+    description: {
+        presence: { allowEmpty: false, message: 'is required' },
+    },
+    floorArea: {
+        presence: { allowEmpty: false, message: 'is required' },
+    },
+    pricePerMonth: {
+        presence: { allowEmpty: false, message: 'is required' },
+    },
+    numberOfRooms: {
+        presence: { allowEmpty: false, message: 'is required' },
+    },
+};
 
 const ApartmentDialog = (props) => {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
     const { title, handleClose } = props;
-    const handleClick = () => {
-        setOpen(!open);
-    };
 
     const [formState, setFormState] = useState({
         isValid: false,
@@ -20,6 +36,41 @@ const ApartmentDialog = (props) => {
         touched: {},
         errors: {},
     });
+
+    useEffect(() => {
+        const errors = validate(formState.values, schema);
+
+        setFormState((formState) => ({
+            ...formState,
+            isValid: errors ? false : true,
+            errors: errors || {},
+        }));
+    }, [formState.values]);
+
+    const hasError = (field) => (formState.touched[field] && formState.errors[field] ? true : false);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
+    const handleSave = () => {
+        const errors = validate(formState.values, schema);
+        setFormState((formState) => {
+            return {
+                ...formState,
+                isValid: errors ? false : true,
+                errors: errors || {},
+                touched: {
+                    name: true,
+                    description: true,
+                    floorArea: true,
+                    pricePerMonth: true,
+                    numberOfRooms: true,
+                },
+            };
+        });
+        if (errors) return;
+    };
 
     const handleChange = (event) => {
         event.persist && event.persist();
@@ -55,6 +106,8 @@ const ApartmentDialog = (props) => {
                                 label="name"
                                 placeholder="name"
                                 name="name"
+                                error={hasError('name')}
+                                helperText={hasError('name') ? formState.errors.name[0] : null}
                                 classes={{ root: classes.normalText }}
                                 onChange={handleChange}
                             />
@@ -65,6 +118,8 @@ const ApartmentDialog = (props) => {
                                 multiline={true}
                                 placeholder="description"
                                 name="description"
+                                error={hasError('description')}
+                                helperText={hasError('description') ? formState.errors.description[0] : null}
                                 classes={{ root: classes.normalText }}
                                 InputProps={{ classes: { input: classes.description } }}
                                 onChange={handleChange}
@@ -75,6 +130,8 @@ const ApartmentDialog = (props) => {
                                 label="Floor Area Size"
                                 name="floorArea"
                                 id="floorArea"
+                                error={hasError('floorArea')}
+                                helperText={hasError('floorArea') ? formState.errors.floorArea[0] : null}
                                 classes={{ root: classes.subArea1 }}
                                 InputProps={{ inputComponent: NumberFormatCustom }}
                                 onChange={handleChange}
@@ -83,15 +140,18 @@ const ApartmentDialog = (props) => {
                                 label="Price Per Month"
                                 name="pricePerMonth"
                                 id="pricePerMonth"
+                                error={hasError('pricePerMonth')}
+                                helperText={hasError('pricePerMonth') ? formState.errors.pricePerMonth[0] : null}
                                 classes={{ root: classes.subArea1 }}
                                 InputProps={{ inputComponent: NumberFormatCustom }}
                                 onChange={handleChange}
-                                prefix="$"
                             />
                             <TextField
                                 label="Number of rooms"
                                 name="numberOfRooms"
                                 id="numberOfRooms"
+                                error={hasError('numberOfRooms')}
+                                helperText={hasError('numberOfRooms') ? formState.errors.numberOfRooms[0] : null}
                                 classes={{ root: classes.subArea1 }}
                                 InputProps={{ inputComponent: NumberFormatCustom }}
                                 onChange={handleChange}
@@ -127,7 +187,7 @@ const ApartmentDialog = (props) => {
                         </div>
                         <RadioGroup
                             defaultValue="Address"
-                            aria-label="gender"
+                            aria-label="geocode"
                             name="geoCodeType"
                             className={classes.formControl}
                             onChange={handleChange}>
@@ -137,10 +197,10 @@ const ApartmentDialog = (props) => {
                     </div>
                 </DialogContent>
                 <DialogActions>
-                    <Button autoFocus onClick={handleClose} color="primary" variant="contained" startIcon={<SaveIcon />}>
+                    <Button autoFocus onClick={handleSave} color="primary" variant="contained" startIcon={<SaveIcon />}>
                         Save
                     </Button>
-                    <Button autoFocus onClick={handleClose} variant="contained">
+                    <Button autoFocus onClick={handleClick} variant="contained">
                         Cancel
                     </Button>
                 </DialogActions>
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -41,7 +41,6 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
     const { errors, isValid } = validateLoginInput(req.body);
-    console.log('>>>', req.body);
     if (!isValid) {
         return res.status(400).json(errors);
     }
